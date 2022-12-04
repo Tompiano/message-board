254f8b4a52030a8faedbdb7ba2ef4ab098cd444c
Tompiano <2022214019@stu.cqupt.edu.cn>
2022-12-04 19:03:56 +0800

    fix(api)更改成restful模式

--- api/router.go
+++ api/router.go
@@ -5,28 +5,27 @@ import "github.com/gin-gonic/gin"
 func Entrance() {
 	r := gin.Default()
 	r.Use(HandlerFunc())
-	u := r.Group("/user")
+	u := r.Group("/User")
 	{
 
-		u.POST("/register", Register) //注册
-		u.GET("/login", Login)        //登录
-		u.GET("/forget", Forget)      //忘记密码可通过保密问题找回
-		u.PUT("/modify", Modify)      //修改密码
+		u.POST("/user", Register) //注册
+		u.GET("/user", Login)     //登录
+		u.GET("/user", Forget)    //忘记密码可通过保密问题找回
+		u.PUT("/user", Modify)    //修改密码
 	}
-	m := r.Group("/message")
+	m := r.Group("Message")
 	{
-		m.POST("/send", SendMessage) //发表留言
-		m.GET("/look", GetMessage)   //查看留言
-		m.PUT("/update", Update)     //修改留言
-		m.DELETE("/delete", Delete)  //删除留言
-		m.POST("/like", Like)        //对留言点赞
+		m.POST("/message", GetMessage) //查看留言
+		m.PUT("/message", Update)      //修改留言
+		m.DELETE("/message", Delete)   //删除留言
+		m.POST("/message", Like)       //对留言点赞
 	}
-	t := r.Group("/comment")
+	t := r.Group("Comment")
 	{
-		t.POST("/send", SendComment)       //发表评论
-		t.GET("/look", LookComment)        //查看评论
-		t.PUT("/modify", ModifyComment)    //修改评论
-		t.DELETE("/delete", DeleteComment) //删除评论
+		t.POST("/comment", SendComment)     //发表评论
+		t.GET("/comment", LookComment)      //查看评论
+		t.PUT("/comment", ModifyComment)    //修改评论
+		t.DELETE("/comment", DeleteComment) //删除评论
 	}
 
 	r.Run()
